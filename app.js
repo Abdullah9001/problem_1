c277f17bbfe29293465069d9a596feb436ec6f44
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-05-19 02:55:24 +0600

    third problem done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let numbers = 6;
 const demoo = document.getElementById("demo");
 const demoo2 = document.getElementById("demo2");
 const demoo3 = document.getElementById("demo3");
+const demoo4 = document.getElementById("demo4");
 
 const printNumber = Math.floor(Math.random() * numbers) + 1;
 
@@ -12,8 +13,6 @@ demoo.innerText = `Ludo random number: ${printNumber}`;
 function randomNumber(max, min) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-console.log(randomNumber(1, 6));
 
 demoo2.innerText = `Ludo random number: ${randomNumber(1, 6)}`;
 // Solved
@@ -39,5 +38,15 @@ for (let i = 0; i < sortingPrint.length; i++) {
   list.innerText = element;
   demoo3.appendChild(list);
 }
-
 // Second Problem done
+
+// third problem
+
+const rollNumber = [5, 6, 9, 15, 10, 11, 4, 3, 8, 1, 2, 7, 12, 13, 14];
+
+function sortingRollFunction(a, b) {
+  return a - b;
+}
+const sortingRoll = rollNumber.sort(sortingRollFunction);
+demoo4.innerText = `Ludo random number: ${sortingRoll}`;
+// third problem done
