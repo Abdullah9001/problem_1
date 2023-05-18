eb0804c136aca7f0f926273ce84b5a95371c21de
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-05-19 01:46:53 +0600

    Secons Problem Done

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// First problem
 let numbers = 6;
 
 const demoo = document.getElementById("demo");
 const demoo2 = document.getElementById("demo2");
+const demoo3 = document.getElementById("demo3");
 
 const printNumber = Math.floor(Math.random() * numbers) + 1;
 
@@ -14,3 +16,37 @@ function randomNumber(max, min) {
 console.log(randomNumber(1, 6));
 
 demoo2.innerText = `Ludo random number: ${randomNumber(1, 6)}`;
+// Solved
+
+// // Second problem
+const students = [
+  "Grace",
+  "Alice",
+  "Frank",
+  "Jack",
+  "David",
+  "Eve",
+  "Charlie",
+  "Bob",
+  "Isaac",
+  "Hannah",
+];
+
+const sNumbers = [87, 7, 94, 73, 9, 93, 8];
+
+function sortingNumbers(a, b) {
+  return a - b;
+}
+
+const SoNuPrint = sNumbers.sort(sortingNumbers);
+console.log(SoNuPrint);
+
+const sortingPrint = students.sort();
+for (let i = 0; i < sortingPrint.length; i++) {
+  const element = sortingPrint[i];
+  const list = document.createElement("li");
+  list.innerText = element;
+  demoo3.appendChild(list);
+}
+
+// Second Problem done
